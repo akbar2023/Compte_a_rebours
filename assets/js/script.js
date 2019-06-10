4bab839be7a940e5a794c1124469904d2196ac98
Akbar <akhan2023@yahoo.com>
2019-06-10 23:50:28 +0200

    Start fonctionne !

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,64 @@
-/* *** SetTimeOut *** */
 
-// let getHour = document.getElementById('hours').value;
-// let getMin = document.getElementById('minutes').value;
-// let getSec = document.getElementById('seconds').value;
-// let timeOut = getHour + ':' + getMin + ':' + getSec; 
+// Function create Select Options
+function creatOptions(){
+    for(var i = 0; i <= 60; i++){
 
-let countdown;
+        let timeArray = ['hours', 'minutes', 'seconds'];
 
-function timer(seconds = 14){
-    const now = Date.now();
-    const then = now + seconds * 1000;
-
-    countdown = setInterval(function(){
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-        // stop it
-        if(secondsLeft <= 0){
-            clearInterval(countdown);
-            return;
+        for(let j = 0; j<= 2; j++){
+            let elm = document.getElementById(timeArray[j]);
+            let newOption = document.createElement('option');
+            elm.append(newOption);
+            newOption.setAttribute('value', i);
+            newOption.innerHTML = i;
         }
+    
+    }
 
-        // display it
-        displayTimeLeft(secondsLeft);
-    }, 1000)
 }
 
-function displayTimeLeft(seconds){
-    const minutes = Math.floor(seconds / 60);
-    const remainderSeconds = seconds % 60;
-    console.log({minutes, remainderSeconds});
-}
+creatOptions(); // execution de la function createOptions
 
-timer();
 
-displayTime()
+
+
+
+
+document.getElementById('start').addEventListener('click', function(){
+
+    let selectHours = document.getElementById('hours');
+    var getHours = selectHours.options[selectHours.selectedIndex].value;
+
+
+    let getMinutes = document.getElementById('minutes').value;
+    let getSeconds = document.getElementById('seconds').value;
+
+    let now = Date.now();
+    let setTime = now+getSeconds*1000+getMinutes*60*1000+getHours*60*60*1000;
+
+    setInterval(function(){ 
+        let chrono = Math.round((setTime - Date.now()) / 1000) + 1; 
+    
+        // chrono donne les bonnes valeurs aux variables correspondants
+        let hours = Math.floor(chrono / 3600);
+        chrono %= 3600;
+        let minutes = Math.floor(chrono / 60);
+        let seconds = chrono % 60;
+        
+        if(chrono < 0){
+            return;
+        }else{
+            // console.log(chrono);
+            // If you want strings with leading zeroes:
+            minutes = String(minutes).padStart(2, "0");
+            hours = String(hours).padStart(2, "0");
+            seconds = String(seconds).padStart(2, "0");
+            // console.log(hours + ":" + minutes + ":" + seconds);
+            document.getElementById('time').innerHTML = hours + "h " + minutes + "m " + seconds + "s";
+        }    
+    }, 1000);
+
+});
+
+
+
