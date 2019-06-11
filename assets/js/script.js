75213da7f0f217eb2c1092a42879dca8297e34f2
Akbar <akhan2023@yahoo.com>
2019-06-11 17:29:07 +0200

    quelques chzngement et correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,21 +34,22 @@ function startTimer(){
         let getSeconds = document.getElementById('seconds').value;
 
         let now = Date.now();
-        let setTime = now+getSeconds*1000+getMinutes*60*1000+getHours*60*60*1000;
+        let setTime = now+getSeconds*1000+getMinutes*60*1000+getHours*3600*1000;
 
         setInterval(function(){ 
             let chrono = Math.round((setTime - Date.now()) / 1000) + 1; 
         
-            // chrono donne les bonnes valeurs aux variables correspondants
+            // chrono donne les bonnes valeurs aux variables correspondantes
             let hours = Math.floor(chrono / 3600);
-            chrono %= 3600;
+            chrono %= 3600; //a corriger
             let minutes = Math.floor(chrono / 60);
             let seconds = chrono % 60;
             
             if(chrono < 0){
                 return;
             }else{
-                if(chrono < 31){
+                if(chrono <= 30){
+                    document.querySelector('circle').style.transitionDuration = '1s';
                     document.querySelector('circle').style.fill = 'white';
                     document.querySelector('circle').style.stroke = 'red';
                     document.querySelector('text').style.stroke = 'red';
@@ -67,6 +68,9 @@ function startTimer(){
 };
 // Fin Timer
 
+// startTimer();
+
+clearInterval(startTimer);
 startTimer();
     
 
